3c20dc278b1c4c8a067a6991e21aae8b3203388f
aoyama <74999140+akuto-aoyama@users.noreply.github.com>
2021-08-15 14:32:17 +0900

    [add]S3保存成功

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,33 @@
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.storage :fog
+#     config.fog_provider = 'fog/aws'
+#     config.fog_directory = 's3-rails-image-20210815'
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#       region: ENV['AWS_REGION'],
+#       path_style: true
+#     }
+#   else
+#     config.storage :file
+#     config.enable_processing = false if Rails.env.test?
+#   end
+# end
+
+# CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = 's3-rails-image-20210815'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      path_style: true
-    }
-  else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
+  }
+  config.fog_public = false # ←コレ
+  config.fog_directory = 's3-rails-image-20210815'
 end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
